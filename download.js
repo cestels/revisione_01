5ed336d85c0ae4898c9fb0414c7b1c8c4ca8b760
cestels <162874498+cestels@users.noreply.github.com>
2024-07-31 10:54:02 +0200

    Add files via upload

--- download.js
+++ download.js
@@ -9,7 +9,7 @@ async function getTemperature() {
         
         if (28.50 <= celsius && celsius <= 29) {
             console.log('yay ci sono riuscita');
-            downloadFile('typeface/', '5-04-VF.ttf');
+            downloadFile('typeface/', 'leporellone.pdf');
         } else if (27.50 < celsius && celsius <= 28) {
             console.log('aiuto speriamo');
             downloadFile('img/ANODE.png', 'ANODE.png');
